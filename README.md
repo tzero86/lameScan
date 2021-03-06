80cd5be069ff18cde7a786e05c91199e278e07f3
tzero86 <eliasmedina86@gmail.com>
2021-03-06 15:23:18 -0300

    second Lame commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# LameScan - a very lame port scanner made with python3
+
+I've decided to start playing with Python3 again, and been putting a bits and pieces 
+of what I lean into creating this basic example of a port scanner.
+
+## Features
+
+- Scans TOP 1000 ports by default (not configurable yet).
+- Supports domain names and IPs by default.
+- Attempts to grab the banner of the port and print the result  
+- User can specify multiple targets separated by commas: 
+    - Example: **facebook.com,192.168.1.1,google.com,127.0.0.1**
+    
+> **NOTE**: Like I mentioned I'm just going through python3 learning process, so don't expect this to become
+a useful tool any time soon.
+
+@tzero86
--- portScanner.py
+++ portScanner.py
@@ -3,20 +3,24 @@ from IPy import IP
 from pyfiglet import Figlet
 from colorama import Fore, Back, Style
 
+VERSION_N = 'v0.0.1'
+RANGE = 1000
+TIMEOUT = 0.2
+
 
 def print_welcome():
     custom_fig = Figlet(font='nancyj')
     print(Fore.GREEN + '------------------------------------------------------------------')
     print(Fore.GREEN + custom_fig.renderText('LameScan'))
-    print(Fore.RED + 'v0.0.1')
-    print(Fore.RED + 'A basic <<Im learning python>> port scanner made made by Tzero86')
+    print(Fore.RED + f'{VERSION_N} A basic <<Im learning python>> port scanner by @Tzero86')
     print(Fore.GREEN + '------------------------------------------------------------------')
+    print(Fore.WHITE)
 
 
 def scan(target):
     converted_ip = check_ip(target)
-    print('\n' + f'[*] Scanning target (TOP 1000 ports): {target}')
-    for port in range(1, 1000):
+    print(Fore.LIGHTBLUE_EX + f'[*] Scanning TOP 1000 ports on the target: {target}')
+    for port in range(1, RANGE):
         scan_port(converted_ip, port)
 
 
@@ -35,14 +39,14 @@ def check_ip(ip):
 def scan_port(r_ip, r_port):
     try:
         sock = socket.socket()
-        sock.settimeout(0.2)
+        sock.settimeout(TIMEOUT)
         sock.connect((r_ip, r_port))
         try:
             banner = get_banner(sock)
             banner = banner.decode().strip("\n")
-            print(f'[Found!] Host: {r_ip} - Port open: {r_port} - Banner: {banner}')
+            print(Fore.GREEN + f'[+] Host: {r_ip} - Port open: {r_port} - Banner: {banner}' + Fore.WHITE)
         except:
-            print(f'[Found!] Host: {r_ip} - Port open: {r_port}')
+            print(Fore.GREEN + f'[+] Host: {r_ip} - Port open: {r_port}' + Fore.WHITE)
     except:
         # print(f'[-] Host: {r_ip} -> Port {r_port} is closed')
         pass
@@ -50,10 +54,11 @@ def scan_port(r_ip, r_port):
 
 print_welcome()
 
-targets = input('[?] Enter the target(s) to scan separated by ,: ')
+targets = input(Fore.LIGHTBLUE_EX + '[?] Enter the target(s) to scan separated by ,: ')
 if ',' in targets:
     for ip_add in targets.split(','):
         scan(ip_add.strip(' '))
 else:
     scan(targets)
 
+print(Fore.LIGHTBLUE_EX + f'[*] LameScan {VERSION_N} has finished processing the task.')

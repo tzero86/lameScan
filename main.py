c57ea13fce015f55de3f4d31c5cd52073b5004d5
tzero86 <eliasmedina86@gmail.com>
2021-03-29 23:36:53 -0300

    Another bunch of fixes, still not working though :) v3

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from libs import vulnScan
 
 # lame = lameScanner.LameScan()
 # lame.new_run()
-# menu = menu.ConfigMenu()
+menu = menu.ConfigMenu()
 
 #  just a test
-# menu.print_options()
+menu.print_options()
 #pprint(lame.scan_results, indent=1, sort_dicts=False)
 
 
 
-vuln = vulnScan.VulnScanner()
+#vuln = vulnScan.VulnScanner()
 
-vuln.new_scan()
+#vuln.new_scan()
